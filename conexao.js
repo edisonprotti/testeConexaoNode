a0558682189ee5a5f5a1c83d3759100cd56c2e27
Edison Protti <38137415+edisonprotti@users.noreply.github.com>
2026-05-21 21:43:38 -0300

    Update conexao.js

--- conexao.js
+++ conexao.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2/promise');
 
-// O Pool precisa ler a porta estritamente da variável de ambiente do Railway
+// Configuração direta para a rede interna protegida do Railway
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST || 'localhost',
-    user: process.env.MYSQLUSER || 'root',
-    password: process.env.MYSQLPASSWORD || '',
-    database: process.env.MYSQLDATABASE || 'railway', // Nome padrão na nuvem
-    port: parseInt(process.env.MYSQLPORT) || 3306, // Converte a string da porta para número
+    host: 'mysql.railway.internal', // O host interno que funcionou no PHP
+    user: 'root',
+    password: 'qvaBoMQcaojxgfFXUzWRkwZqOTvMxurd', // A senha do seu banco
+    database: 'railway', // Nome do banco padrão na nuvem
+    port: 3306, // Porta interna padrão do container MySQL
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
