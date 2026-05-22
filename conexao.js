45e327340095b1f8601785a778ec4c9e61614c54
Edison Protti <38137415+edisonprotti@users.noreply.github.com>
2026-05-21 21:36:42 -0300

    Update conexao.js

--- conexao.js
+++ conexao.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2/promise');
 
-// O Node lê as variáveis da memória do Railway ou adota o padrão local
+// O Pool precisa ler a porta estritamente da variável de ambiente do Railway
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST || 'localhost',
     user: process.env.MYSQLUSER || 'root',
     password: process.env.MYSQLPASSWORD || '',
-    database: process.env.MYSQLDATABASE || 'aulaphp',
-    port: process.env.MYSQLPORT || 3306,
+    database: process.env.MYSQLDATABASE || 'railway', // Nome padrão na nuvem
+    port: parseInt(process.env.MYSQLPORT) || 3306, // Converte a string da porta para número
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
