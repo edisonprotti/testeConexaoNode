3f86850b8e433b3debc21924f0f85e92504010b7
Edison Protti <38137415+edisonprotti@users.noreply.github.com>
2026-05-21 21:47:41 -0300

    Update conexao.js

--- conexao.js
+++ conexao.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 
 // Configuração direta para a rede interna protegida do Railway
 const pool = mysql.createPool({
-    host: 'mysql.railway.internal', // O host interno que funcionou no PHP
+    host: 'autorack.proxy.rlwy.net', // O host interno que funcionou no PHP
     user: 'root',
     password: 'qvaBoMQcaojxgfFXUzWRkwZqOTvMxurd', // A senha do seu banco
     database: 'railway', // Nome do banco padrão na nuvem
-    port: 3306, // Porta interna padrão do container MySQL
+    port: 12345, // Porta interna padrão do container MySQL
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
